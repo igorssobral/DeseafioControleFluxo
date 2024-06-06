7ad58e05c7ca97e04d66c50c3963426a5307ffde
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-06-06 12:37:48 -0300

    Desafio concluído

---
+++ Contador.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws InterruptedException {
+        
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        terminal.close();
+        try {
+            // chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException, InterruptedException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        // realizar o for para imprimir os números com base na variável contagem
+
+        for (int i = 1; i <= contagem; i++) {
+
+            System.out.println("Imprimindo o número " + i);
+            Thread.sleep(500);
+        }
+    }
+}
